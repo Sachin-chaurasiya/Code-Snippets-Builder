26f28eb555840ca2dd17012320020a1ee24e6da5
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-05-15 01:22:34 +0530

    clean up

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import CodeEditor from '../../components/CodeEditor/CodeEditor';
-import AddMediaModal from '../../components/AddMediaModal/AddMediaModal';
 
 const Home = () => {
   return (
     <>
-      <AddMediaModal />
       <CodeEditor />;
     </>
   );
--- src/components/CustomNodes/ImageNode.tsx
+++ src/components/CustomNodes/ImageNode.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { FC } from 'react';
 import AddMediaModal from '../AddMediaModal/AddMediaModal';
+import { Handle, NodeProps, Position } from 'reactflow';
 
-const ImageNode = () => {
-  return <AddMediaModal />;
+const ImageNode: FC<NodeProps> = ({ selected }) => {
+  return (
+    <>
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{ background: '#555', opacity: 0 }}
+        onConnect={(params) => console.log('handle onConnect', params)}
+        isConnectable={false}
+      />
+      <AddMediaModal />
+      <Handle
+        type="source"
+        position={Position.Right}
+        style={{ background: '#555', opacity: 0 }}
+        onConnect={(params) => console.log('handle onConnect', params)}
+        isConnectable={false}
+      />
+      ;
+    </>
+  );
 };
 
 export default ImageNode;
