84494ab8be5f5774a6075e0986c43052775a7714
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-05-20 13:01:39 +0530

    fix: favicon and brand logo

--- src/components/Sidebar/LeftSideBar.tsx
+++ src/components/Sidebar/LeftSideBar.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
-import { Box, Flex, useColorModeValue, Text, BoxProps } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  useColorModeValue,
+  Text,
+  BoxProps,
+  Image,
+} from '@chakra-ui/react';
 import { FiHome, FiEdit } from 'react-icons/fi';
 import { IconType } from 'react-icons';
 import NavItem from './NavItem';
 import { ROUTES } from 'constant';
-import { AiFillCode } from 'react-icons/ai';
+import LogoImage from 'assets/images/logo.png';
 
 interface LinkItemProps {
   name: string;
@@ -28,9 +35,11 @@ const LeftSidebar: FC<BoxProps> = () => {
       h="full"
       shadow="md"
     >
-      <Flex h="20" alignItems="center" mx="8" gap={2}>
-        <AiFillCode color="#2d3748" fontSize={32} />
+      <Flex h="20" alignItems="center" gap={2}>
+        <Image boxSize="64px" src={LogoImage} objectFit="contain" />
         <Text
+          ml="-16px"
+          mb="4px"
           fontSize="xl"
           fontWeight="bold"
           backgroundImage="linear-gradient(270deg, rgb(20, 30, 48), rgb(36, 59, 85))"
