5cf3da2dc0875b2d5220fb787c57aee9c3c22c08
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-06-08 13:16:16 +0530

    refactor(ui): editor components (#30)

---
+++ src/components/Editor/Configs/BackgroundConfig.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormLabel,
+  SimpleGrid,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { useAppProvider } from 'AppProvider';
+import CustomSwitch from 'components/Common/CustomSwitch/CustomSwitch';
+import { GRADIENT_COLORS } from 'constants/editor';
+import { COMMON_TEXT_PROPS } from 'constants/text';
+import React from 'react';
+import { BiCheckCircle } from 'react-icons/bi';
+
+const BackgroundConfig = () => {
+  const { background, onUpdateBackground, hideWaterMark, onUpdateWaterMark } =
+    useAppProvider();
+
+  return (
+    <Box alignItems="flex-start" as={VStack} mb={4}>
+      <Text fontSize="lg" fontWeight="bold" {...COMMON_TEXT_PROPS}>
+        Background
+      </Text>
+      <Divider />
+      <FormControl display="flex" alignItems="center">
+        <FormLabel htmlFor="water-mark" mb="0">
+          Hide watermark
+        </FormLabel>
+        <CustomSwitch
+          id="water-mark"
+          isChecked={hideWaterMark}
+          onChange={(e) => {
+            onUpdateWaterMark(e.target.checked);
+          }}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Fill</FormLabel>
+        <SimpleGrid columns={5} gap={4}>
+          {GRADIENT_COLORS.map((c) => (
+            <Button
+              key={c}
+              aria-label={c}
+              background={c}
+              height="40px"
+              width="40px"
+              padding={0}
+              minWidth="unset"
+              borderRadius={3}
+              _hover={{ background: c }}
+              onClick={() => {
+                onUpdateBackground(c);
+              }}>
+              {background === c ? (
+                <BiCheckCircle
+                  color="white"
+                  style={{ display: 'block', margin: 'auto' }}
+                />
+              ) : (
+                <></>
+              )}
+            </Button>
+          ))}
+        </SimpleGrid>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default BackgroundConfig;
---
+++ src/components/Editor/Panels/WatermarkPanel.tsx
@@ -0,0 +1,29 @@
+import { useAppProvider } from 'AppProvider';
+import BrandLogo from 'components/Common/BrandLogo/BrandLogo';
+import React, { Fragment } from 'react';
+import { Panel } from 'reactflow';
+
+const WatermarkPanel = () => {
+  const { hideWaterMark } = useAppProvider();
+  return (
+    <Fragment>
+      {!hideWaterMark ? (
+        <Panel position="bottom-right">
+          <BrandLogo
+            gap={4}
+            h="auto"
+            textColor="white"
+            logoType="light"
+            textBackgroundColor="transparent"
+            backgroundClip="inherit"
+            logoSize="x-small"
+            textFontSize="md"
+            textFontWeight="normal"
+          />
+        </Panel>
+      ) : null}
+    </Fragment>
+  );
+};
+
+export default WatermarkPanel;
