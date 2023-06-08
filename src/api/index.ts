1e11b640969b9fb9e25c8500154d5e0876b42f74
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-06-08 20:41:34 +0530

    chore(backend): add helper method to apis

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { Account, Client, Databases } from 'appwrite';
 import { Server } from 'utils/Config';
+import { getUniqueId } from 'utils/EditorUtils';
 
 const initClient = () => {
   const client = new Client()
@@ -12,4 +13,52 @@ const initClient = () => {
   return { database, account };
 };
 
-export const API_CLIENT = initClient();
+export const API_CLIENT = {
+  ...initClient(),
+  getLoggedInUserSession: async () => {
+    try {
+      const session = await API_CLIENT.account.getSession('current');
+
+      return session;
+    } catch (error) {
+      return undefined;
+    }
+  },
+  googleLogin: () => {
+    API_CLIENT.account.createOAuth2Session(
+      'google',
+      `${window.location.origin}/profile`,
+      `${window.location.origin}/login`
+    );
+  },
+  githubLogin: () => {
+    API_CLIENT.account.createOAuth2Session(
+      'github',
+      `${window.location.origin}/profile`,
+      `${window.location.origin}/login`
+    );
+  },
+  emailLogin: async (email: string, password: string, name?: string) => {
+    try {
+      const user = await API_CLIENT.account.create(
+        getUniqueId(),
+        email,
+        password,
+        name
+      );
+
+      return user;
+    } catch (error) {
+      // handle error
+
+      return undefined;
+    }
+  },
+  logout: async () => {
+    try {
+      await API_CLIENT.account.deleteSession('current');
+    } catch (error) {
+      // handle error
+    }
+  },
+};
