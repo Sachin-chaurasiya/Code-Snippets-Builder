ab9267e41f039cfbf1f5351ce490937f2d2d345f
Anujkumar Yadav <anujf0510@gmail.com>
2023-06-20 23:36:46 +0530

    added other contributors link in footer (#49)

--- src/components/Common/Footer/Footer.tsx
+++ src/components/Common/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Box, Link, Text } from '@chakra-ui/react';
-import { PERSONAL_WEBSITE } from 'constants/links';
+import { GITHUB_CONTRIBUTORS, PERSONAL_WEBSITE } from 'constants/links';
 import React from 'react';
 
 const Footer = () => {
@@ -7,7 +7,9 @@ const Footer = () => {
     <Box as="footer" bg="gray.100" py={4}>
       <Box maxWidth="container.md" mx="auto" textAlign="center">
         <Text>
-          Made with ❤️ by <Link href={PERSONAL_WEBSITE}>Sachin Chaurasiya</Link>
+          Made with ❤️ by{' '}
+          <Link href={PERSONAL_WEBSITE}>Sachin Chaurasiya </Link>
+          and <Link href={GITHUB_CONTRIBUTORS}>Folks</Link>{' '}
         </Text>
       </Box>
     </Box>
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -3,3 +3,5 @@ import { ROUTES } from './common';
 export const PERSONAL_WEBSITE = 'https://sachinchaurasiya.dev';
 export const SUCCESS_REDIRECT_URL = `${window.location.origin}${ROUTES.CALLBACK}`;
 export const FAILURE_REDIRECT_URL = `${window.location.origin}${ROUTES.SIGN_IN}`;
+export const GITHUB_CONTRIBUTORS =
+  'https://github.com/Sachin-chaurasiya/Code-Snippets-Builder/graphs/contributors';
