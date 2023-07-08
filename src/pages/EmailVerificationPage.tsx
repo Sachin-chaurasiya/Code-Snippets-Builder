b405c5d1a9797bde0b0683e8ba307a15ff84a1ea
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-07-08 14:38:13 +0530

    chore: update verification state

--- src/pages/EmailVerificationPage.tsx
+++ src/pages/EmailVerificationPage.tsx
@@ -1,12 +1,14 @@
 import { useToast } from '@chakra-ui/react';
+import { useAppProvider } from 'AppProvider';
 import { API_CLIENT } from 'api';
-import { AppwriteException } from 'appwrite';
+import { AppwriteException, Models } from 'appwrite';
 import Loader from 'components/Common/Loader/Loader';
 import { ROUTES } from 'constants/common';
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const EmailVerificationPage = () => {
+  const { handleUpdateLoggedInUser, loggedInUser } = useAppProvider();
   const toast = useToast();
   const location = useLocation();
   const navigate = useNavigate();
@@ -14,6 +16,10 @@ const EmailVerificationPage = () => {
   const handleUpdateVerification = async (userId: string, secret: string) => {
     try {
       await API_CLIENT.account.updateVerification(userId, secret);
+      handleUpdateLoggedInUser({
+        ...(loggedInUser as Models.User<Models.Preferences>),
+        emailVerification: true,
+      });
       navigate(ROUTES.DASHBOARD);
       toast({
         description: 'Email verified successfully!',
