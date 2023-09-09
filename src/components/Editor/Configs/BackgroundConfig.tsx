30c427ffd3b28c63d66858573ca0feefdcb1d0f7
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-09-09 12:27:09 +0530

    feat: add support for transparent bg (#74)
    * feat: add support for transparent bg
    
    * chore: address comment

--- src/components/Editor/Configs/BackgroundConfig.tsx
+++ src/components/Editor/Configs/BackgroundConfig.tsx
@@ -6,11 +6,13 @@ import {
   FormLabel,
   SimpleGrid,
   Text,
+  Tooltip,
   VStack,
 } from '@chakra-ui/react';
 
 import CustomSwitch from 'components/Common/CustomSwitch/CustomSwitch';
-import { GRADIENT_COLORS } from 'constants/editor';
+import { BORDER_RADIUS_MEDIUM } from 'constants/common';
+import { GRADIENT_COLORS, TRANSPARENT } from 'constants/editor';
 import { COMMON_TEXT_PROPS } from 'constants/text';
 import { EditorSidebarProps } from 'interfaces/Editor.interface';
 import React, { FC } from 'react';
@@ -43,29 +45,35 @@ const BackgroundConfig: FC<
       <FormControl>
         <FormLabel>Fill</FormLabel>
         <SimpleGrid columns={5} gap={4}>
-          {GRADIENT_COLORS.map((c) => (
-            <Button
-              key={c}
-              aria-label={c}
-              background={c}
-              height="40px"
-              width="40px"
-              padding={0}
-              minWidth="unset"
-              borderRadius={3}
-              _hover={{ background: c }}
-              onClick={() => {
-                onUpdateBackground(c);
-              }}>
-              {background === c ? (
-                <BiCheckCircle
-                  color="white"
-                  style={{ display: 'block', margin: 'auto' }}
-                />
-              ) : (
-                <></>
-              )}
-            </Button>
+          {GRADIENT_COLORS.map((bgColor) => (
+            <Tooltip
+              key={bgColor}
+              label={bgColor}
+              borderRadius={BORDER_RADIUS_MEDIUM}>
+              <Button
+                aria-label={bgColor}
+                background={bgColor}
+                height="40px"
+                width="40px"
+                padding={0}
+                minWidth="unset"
+                borderRadius={3}
+                _hover={{ background: bgColor }}
+                border={bgColor === TRANSPARENT ? '1px' : 'none'}
+                borderColor={bgColor === TRANSPARENT ? 'gray.200' : TRANSPARENT}
+                onClick={() => {
+                  onUpdateBackground(bgColor);
+                }}>
+                {background === bgColor ? (
+                  <BiCheckCircle
+                    color={bgColor === TRANSPARENT ? 'black' : 'white'}
+                    style={{ display: 'block', margin: 'auto' }}
+                  />
+                ) : (
+                  <></>
+                )}
+              </Button>
+            </Tooltip>
           ))}
         </SimpleGrid>
       </FormControl>
