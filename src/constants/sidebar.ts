cc21a186dfb538074066905fcaad58fad4a22d79
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-11-12 00:41:11 +0530

    feat(#87): add sorting and pagination for snippets

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -2,8 +2,10 @@ import { LinkItemProps } from 'components/Sidebar/Sidebar.interface';
 import { FiHome } from 'react-icons/fi';
 import { ROUTES } from 'constants/common';
 import { RxDashboard } from 'react-icons/rx';
+import { TbTemplate } from 'react-icons/tb';
 
 export const LINK_ITEMS: LinkItemProps[] = [
   { name: 'Home', icon: FiHome, path: ROUTES.HOME },
   { name: 'Dashboard', icon: RxDashboard, path: ROUTES.DASHBOARD },
+  { name: 'Templates', icon: TbTemplate, path: ROUTES.TEMPLATES },
 ];
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,17 +1,15 @@
-import { Box, Button, Divider, Heading, Stack } from '@chakra-ui/react';
+import { Box, Heading, Stack } from '@chakra-ui/react';
 import { useAppProvider } from 'AppProvider';
 
 import EmailVerification from 'components/Common/EmailVerification';
 import Loader from 'components/Common/Loader/Loader';
 import SnippetList from 'components/SnippetList/SnippetList';
-import SnippetTemplates from 'components/SnippetTemplates/SnippetTemplates';
 import { BORDER_RADIUS_LARGE } from 'constants/common';
 
 import React from 'react';
-import { MdOutlineTour } from 'react-icons/md';
 
 const DashboardPage = () => {
-  const { loggedInUser, isFetchingUser, onStartTour } = useAppProvider();
+  const { loggedInUser, isFetchingUser } = useAppProvider();
 
   if (isFetchingUser) {
     return <Loader />;
@@ -21,14 +19,9 @@ const DashboardPage = () => {
     <Box bg="white" minH="100vh" borderRadius={BORDER_RADIUS_LARGE} p={4}>
       {loggedInUser?.emailVerification ? (
         <Stack spacing={6}>
-          <Stack justifyContent="space-between" direction="row">
-            <Heading>Your Snippets</Heading>
-            <Button leftIcon={<MdOutlineTour />} onClick={onStartTour}>
-              Tour
-            </Button>
-          </Stack>
-          <SnippetTemplates />
-          <Divider variant="dashed" />
+          <Heading as="h4" size="md">
+            Your Snippets
+          </Heading>
           <SnippetList />
         </Stack>
       ) : (
