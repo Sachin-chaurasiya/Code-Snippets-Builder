15b75e6316d181a2ecc75d15fbaa46b8477e48b8
anuj-kumary <anujf0510@gmail.com>
2024-03-30 14:15:14 +0530

    Revamp footer component

--- src/components/Common/Footer/Footer.tsx
+++ src/components/Common/Footer/Footer.tsx
@@ -1,74 +1,83 @@
 import {
   Box,
-  Button,
-  Grid,
-  Link as ChakraLink,
   Stack,
+  HStack,
+  VStack,
+  Link as ChakraLink,
+  Divider,
   Text,
+  Button,
 } from '@chakra-ui/react';
-import { GITHUB_LINK, TWITTER_LINK } from 'constants/links';
 import { BsGithub, BsTwitter } from 'react-icons/bs';
 import BrandLogo from '../BrandLogo/BrandLogo';
 import { ROUTES } from 'constants/common';
 import { Link } from 'react-router-dom';
+import { GITHUB_LINK, TWITTER_LINK } from 'constants/links';
 
 const Footer = () => {
   return (
-    <Box as="footer" bg="brand.500" py={8} textColor="white">
-      <Grid
-        templateColumns={{
-          lg: 'repeat(3, 1fr)',
-          md: 'repeat(2, 1fr)',
-          sm: 'repeat(1, 1fr)',
-        }}
-        gap={8}
-        px={8}
-        pb={4}>
-        <Box>
-          <Stack spacing={0}>
-            <BrandLogo ml="-10px" textColor="white" cursor="pointer" />
-            <Text>
+    <>
+      <Box bg="brand.500" p={{ base: 5, md: 8 }} width="100vw">
+        <Stack
+          spacing={{ base: 8, md: 0 }}
+          justifyContent="space-between"
+          direction={{ base: 'column', md: 'row' }}
+          marginBottom={8}>
+          <Box maxW="500px">
+            <BrandLogo textColor="white" cursor="pointer" />
+            <Text textColor="white" mt={2} fontSize="md">
               Drag and drop code snippet builder to showcase your code in a
               beautiful and professional manner.
             </Text>
-          </Stack>
-        </Box>
-        <Box>
-          <Stack spacing={2}>
-            <Text fontWeight="bold">Quick Links</Text>
-            <Link to={ROUTES.HOME}>Home</Link>
-            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-            <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-          </Stack>
-        </Box>
-        <Box>
-          <Stack spacing={2}>
-            <Text fontWeight="bold">Social Links</Text>
-            <ChakraLink href={TWITTER_LINK} target="_blank">
-              <Button
-                pl={0}
-                leftIcon={<BsTwitter />}
-                variant="ghost"
-                colorScheme="black">
-                Twitter
-              </Button>
-            </ChakraLink>
-            <ChakraLink href={GITHUB_LINK} target="_blank">
-              <Button
-                pl={0}
-                leftIcon={<BsGithub />}
-                variant="ghost"
-                colorScheme="black">
-                GitHub
-              </Button>
-            </ChakraLink>
-          </Stack>
-        </Box>
-      </Grid>
-      <Text textAlign="center">
-        © {new Date().getFullYear()} SnippetBuilder. All rights reserved.
-      </Text>
-    </Box>
+          </Box>
+          <HStack
+            spacing={4}
+            justifyContent={{ sm: 'space-between', md: 'normal' }}>
+            <VStack spacing={4} alignItems="flex-start">
+              <Text textColor="white" fontSize="md" fontWeight="bold">
+                Quick Links
+              </Text>
+              <VStack spacing={2} alignItems="flex-start" color="white">
+                <Link to={ROUTES.HOME}>Home</Link>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+              </VStack>
+            </VStack>
+          </HStack>
+          <HStack>
+            <VStack spacing={4} alignItems="flex-start">
+              <Text textColor="white" fontSize="md" fontWeight="bold">
+                Social Links
+              </Text>
+              <VStack spacing={2} alignItems="flex-start" color="white">
+                <ChakraLink href={TWITTER_LINK} target="_blank">
+                  <Button
+                    pl={0}
+                    leftIcon={<BsTwitter />}
+                    variant="ghost"
+                    colorScheme="black">
+                    Twitter
+                  </Button>
+                </ChakraLink>
+                <ChakraLink href={GITHUB_LINK} target="_blank">
+                  <Button
+                    pl={0}
+                    leftIcon={<BsGithub />}
+                    variant="ghost"
+                    colorScheme="black">
+                    GitHub
+                  </Button>
+                </ChakraLink>
+              </VStack>
+            </VStack>
+          </HStack>
+        </Stack>
+        <Divider my={8} />
+        <Text textColor="white" textAlign="center">
+          © {new Date().getFullYear()} SnippetBuilder. All rights reserved.
+        </Text>
+      </Box>
+    </>
   );
 };
 
